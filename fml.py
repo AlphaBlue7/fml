0ffc92cbb783f58f753b95240c563decbb4ec8ff
Yw06QZxhpI46 <arjunsankarm77@gmail.com>
2020-03-17 14:58:13 +0530

    First Commit

---
+++ fml.py
@@ -0,0 +1,72 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 22:45:29) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #!/usr/bin/python
+
+class Interests:
+
+   people = 0
+
+   def __init__(self, dancing,singing,reading,binging):
+      self.dancing =0
+      self.singing =0
+      self.binging=0
+      Interests.people += 1
+   
+   
+"This would create first object of Employee class"
+Interests.emp1[5000] 
+
+class individuals:
+    
+    def __init__(self, mentor,time):
+        mentor=0
+        time=0
+individuals.status[5000]
+   
+g=0
+b=0
+def addStacks():
+    limit=0
+    number=0
+    limit=input("Enter no. of people")
+    
+    while (number<limit):
+        emp1[number].dancing=input("Enter 1 if you're interested in dancing else 0 \n") 
+        emp1[number].singing=input("Enter 1 if you're interested in singing else 0 \n") 
+        emp1[number].binging=input("Enter 1 if you're interested in binging else 0") 
+        number+=1
+    b=number-1
+
+def setMentorOrLearner() :
+    count=0
+    limit2=input("Enter no. of people")
+    
+    while (count<limit):
+        status[count]=input("Enter 1 if you're a mentor else 0 \n") 
+        if(status[count]==1):g+=1
+        
+        count+=1
+
+def setAvailableTime() :
+    count2=0
+    time2[g]
+    while(count2<g):
+        status[count2].time= input('enter time available \n')
+        count2+=1
+         
+
+
+def getMentor():
+    count3=0
+    count4=0
+    timeavail[g]
+    while(count3<g):
+        timeavail[count3]=input('enter suitable time')
+        while(count4<g):
+            if(timeavail==status[count4].time):
+                print ('mentor found, time is comfortable')
+                break
+            
+        if(count4==g): print('sorry mentor not available')
+        count4=0 
+        break
